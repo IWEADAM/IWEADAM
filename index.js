8f9f400fea84a06404a22b0c8abe10c3c9cd023b
IWEADAM <143907334+IWEADAM@users.noreply.github.com>
2025-11-16 16:16:04 +0200

    ج

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hi");
+fetch("https://discord.com/api/webhooks/1439619216624844812/lo-HnJWV36Rmhf0OCOZASToXf7uclpnbREDl_m28ojU72Pskl3VZKIDdynFTJfjrmagDL", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    content: `New message: ${message}`
+  })
+});
