d185ff08c9f4aa641037271d06922f216da596c9
IWEADAM <143907334+IWEADAM@users.noreply.github.com>
2025-11-16 16:28:24 +0200

    DONE

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-fetch("https://discord.com/api/webhooks/1439619216624844812/lo-HnJWV36Rmhf0OCOZASToXf7uclpnbREDl_m28ojU72Pskl3VZKIDdynFTJfjrmagDL", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({
-    content: `New message: ${message}`
-  })
-});
+document.getElementById("sendBtn").addEventListener("click", sendMessage);
+
+function sendMessage() {
+    const message = document.getElementById("msgInput").value;
+
+    if (!message.trim()) return; // لو فاضي متبعتش
+
+    fetch("https://discord.com/api/webhooks/1439619216624844812/lo-HnJWV36Rmhf0OCOZASToXf7uclpnbREDl_m28ojU72Pskl3VZKIDdynFTJfjrmagD", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            content: `New message: ${message}`
+        })
+    })
+    .then(res => {
+        console.log("sent");
+        document.getElementById("msgInput").value = ""; // يمسح التكست
+    })
+    .catch(err => console.log("error:", err));
